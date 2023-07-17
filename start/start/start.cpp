ee9bd6b3a2bc04f8525847fd691002c2c7e0d089
VadimLitvin1 <138385101+VadimLitvin1@users.noreply.github.com>
2023-07-17 07:54:46 +0300

    .cpp

--- start/start/start.cpp
+++ start/start/start.cpp
@@ -1,24 +1,24 @@
-﻿#include <iostream>
+#include <iostream>
 
 void showMainMenu()
 {
-    std::cout << "Music Service\n";
-    std::cout << "1. Select Music\n";
-    std::cout << "2. Profile\n";
-    std::cout << "3. Exit\n";
-    std::cout << "Choose an option: ";
+    std::cout << "Music Service\n"; // Головне меню сервісу
+    std::cout << "1. Select Music\n"; // Опція вибору музики
+    std::cout << "2. Profile\n"; // Опція перегляду профілю
+    std::cout << "3. Exit\n"; // Опція виходу
+    std::cout << "Choose an option: "; // Виберіть опцію
 }
 
 void selectMusic()
 {
-    std::cout << "Music selected\n";
-    // You can add logic for selecting music here
+    std::cout << "Music selected\n"; // Вибрано музику
+    // Тут можна додати логіку для вибору музики
 }
 
 void viewProfile()
 {
-    std::cout << "Profile\n";
-    // You can add logic for viewing the profile here
+    std::cout << "Profile\n"; // Профіль
+    // Тут можна додати логіку для перегляду профілю
 }
 
 int main()
@@ -28,29 +28,29 @@ int main()
 
     while (!exit)
     {
-        showMainMenu();
-        std::cin >> option;
+        showMainMenu(); // Виведення головного меню
+        std::cin >> option; // Зчитування вибору користувача
 
         switch (option)
         {
         case 1:
-            selectMusic();
+            selectMusic(); // Виклик функції вибору музики
             break;
         case 2:
-            viewProfile();
+            viewProfile(); // Виклик функції перегляду профілю
             break;
         case 3:
-            exit = true;
+            exit = true; // Встановлення прапорця вийти з циклу
             break;
         default:
-            std::cout << "Invalid choice. Please try again.\n";
+            std::cout << "Invalid choice. Please try again.\n"; // Повідомлення про невірний вибір
             break;
         }
 
         std::cout << std::endl;
     }
 
-    std::cout << "Thank you for using the music service!\n";
+    std::cout << "Thank you for using the music service!\n"; // Повідомлення про завершення програми
 
     return 0;
 }
